86cea0c49fdc86d3733601d3988b21f58381dfc5
kiet <chungkiet31@gmail.com>
2025-03-27 19:21:12 +0700

    GMSDR

--- models/model.py
+++ models/model.py
@@ -10,10 +10,10 @@ def create_model(model_name, loss, conf, device,  support=None):
     support = torch.tensor(support, dtype=torch.float32)  
     if model_name == 'CCRNN':
         model = EvoNN2(**conf, support=support, device=device)
-        return model, MetricNNTrainer(model, loss)
+        return model, MetricNNTrainer(model, loss, model_name)
     if model_name == 'GMSDR':
         model = GMSDRNet(**conf, support=support, device=device)
-        return model, MetricNNTrainer(model, loss)
+        return model, MetricNNTrainer(model, loss, model_name)
 
 
 
@@ -28,15 +28,20 @@ class Trainer:
 
 
 class MetricNNTrainer(Trainer):
-    def __init__(self, model, loss):
+    def __init__(self, model, loss, model_name=None):
         super(MetricNNTrainer, self).__init__(model, loss)
+        self.model_name = model_name
         self.train_batch_seen: int = 0
 
     def train(self, inputs: Tensor, targets: Tensor, phase: str):
-        if phase == 'train':
-            self.train_batch_seen += 1
-        i_targets = targets if phase == 'train' else None
-        outputs, graph = self.model(inputs, i_targets, self.train_batch_seen)
+        outputs, graph = self.model(inputs)
         loss = self.loss(outputs, targets, graph)
+        if self.model_name == 'CCRNN':
+            if phase == 'train':
+                self.train_batch_seen += 1
+            i_targets = targets if phase == 'train' else None
+            outputs, graph = self.model(inputs, i_targets, self.train_batch_seen)
+            loss = self.loss(outputs, targets, graph)
+ 
         return outputs, loss
 
